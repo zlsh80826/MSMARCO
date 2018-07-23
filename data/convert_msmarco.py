21a919bc6645160d6824fde958aa09e1f725d3e2
zlsh80826 <zlsh80826@gmail.com>
2018-07-23 23:20:00 +0800

    add ratio argument to avoid large dev data

--- data/convert_msmarco.py
+++ data/convert_msmarco.py
@@ -8,6 +8,7 @@ import numpy as np
 import multiprocessing
 import time
 import argparse
+import random
 from utils import smith_waterman
 
 def preprocess(s):
@@ -16,7 +17,7 @@ def preprocess(s):
 def tokenize(s, context_mode=False ):
     nltk_tokens=[t.replace("''", '"').replace("``", '"') for t in nltk.word_tokenize(s)]
     additional_separators = (
-             "-", "\u2212", "\u2014", "\u2013", "/", "~", '"', "'", "\u201C", "\u2019", "\u201D", "\u2018", "\u00B0")
+        "-", "\u2212", "\u2014", "\u2013", "/", "~", '"', "'", "\u201C", "\u2019", "\u201D", "\u2018", "\u00B0")
     tokens = []
     for token in nltk_tokens:
         tokens.extend([t for t in (re.split("([{}])".format("".join(additional_separators)), token)
@@ -45,7 +46,9 @@ def process(i, j, is_test):
 
     temp = ' '.join(pp['passage_text'] for pp in p)
     if yesno:
-        temp += ' Yes No Answer Present .'
+        temp += ' Yes No Answer Present.'
+    else:
+        temp += ' No Answer Present.'
     context = preprocess(temp)
 
     ctokens = trim_empty(tokenize(context, context_mode=True))
@@ -58,35 +61,39 @@ def process(i, j, is_test):
             answer = preprocess(a)
             if answer == '':
                 bad = True
-            atokens = trim_empty(tokenize(answer, context_mode=True))
-            normalized_answer = ' '.join(atokens).lower()
-            normalized_context_lower = normalized_context.lower()
-            pos = normalized_context_lower.find(normalized_answer)
-            if pos >= 0:
-                start = bisect.bisect(np.cumsum([1 + len(t) for t in nctokens]), pos)
-                end = start + len(atokens)
-            else:
-                natokens = normalized_answer.split()
-                try:
-                    (start, end), (_, _), score = smith_waterman(normalized_context_lower.split(), natokens)
-                    start -= 1
-                    ratio = 0.5 * score / min(len(nctokens), len(natokens))
-                    if ratio < 0.8:
-                        bad = True
-                except:
-                    bad = True
+
             if not bad:
-                output = [str(j['query_id']), j['query_type'], 
+                atokens = trim_empty(tokenize(answer, context_mode=True))
+                normalized_answer = ' '.join(atokens).lower()
+                normalized_context_lower = normalized_context.lower()
+                pos = normalized_context_lower.find(normalized_answer)
+                if pos >= 0:
+                    start = bisect.bisect(np.cumsum([1 + len(t) for t in nctokens]), pos)
+                    end = start + len(atokens)
+                    if len(nctokens) < end:
+                        bad = True
+                else:
+                    natokens = normalized_answer.split()
+                    try:
+                        (start, end), (_, _), score = smith_waterman(normalized_context_lower.split(), natokens)
+                        start -= 1
+                        ratio = 0.5 * score / min(len(nctokens), len(natokens))
+                        if ratio < 0.8:
+                            bad = True
+                    except:
+                        bad = True
+                if not bad:
+                    output = [str(j['query_id']), j['query_type'], 
                           ' '.join(nctokens), ' '.join(qtokens),
                           ' '.join(nctokens[start:end]), str(start), str(end)]
-                outputs.append(output)
+                    outputs.append(output)
     else:
         output = [str(j['query_id']), j['query_type'], ' '.join(nctokens),' '.join(qtokens)]
         outputs.append(output)
         
     return outputs
                 
-def convert(file_name, outfile, is_test, num_threads, version):
+def convert(file_name, outfile, is_test, num_threads, version, ratio):
     print('Generating', outfile, '...')
     start = time.perf_counter()
 
@@ -103,14 +110,17 @@ def convert(file_name, outfile, is_test, num_threads, version):
             raw_data = json.load(f)
 
         for id_ in raw_data['query_id']:
-            dict_ = dict()
-            dict_['passages'] = raw_data['passages'][id_]
-            dict_['query'] = raw_data['query'][id_]
-            dict_['query_id'] = id_
-            dict_['query_type'] = raw_data['query_type'][id_]
-            if not is_test:
-                dict_['answers'] = raw_data['answers'][id_]
-            data.append(dict_)
+            select = random.random() <= ratio
+
+            if file_name.find('dev') < 0 or select:
+                dict_ = dict()
+                dict_['passages'] = raw_data['passages'][id_]
+                dict_['query'] = raw_data['query'][id_]
+                dict_['query_id'] = id_
+                dict_['query_type'] = raw_data['query_type'][id_]
+                if not is_test:
+                    dict_['answers'] = raw_data['answers'][id_]
+                data.append(dict_)
     else:
         raise NotImplementedError
 
@@ -137,18 +147,19 @@ def convert(file_name, outfile, is_test, num_threads, version):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert MSMARCO raw data to tsv format')
     parser.add_argument('--threads', help='Number of threads to multi-preprocessing', default=1, type=int)
+    parser.add_argument('--ratio', help='Ratio of dev data', default=1, type=float)
     parser.add_argument('version', choices=['v1', 'v2'])
     args = vars(parser.parse_args())
 
     if args['version'] == 'v1':
-        convert('v1/train.json.gz', 'train.tsv', False, args['threads'], args['version'])
-        convert('v1/dev.json.gz', 'dev.tsv', False, args['threads'], args['version'])
-        convert('v1/test.json.gz', 'test.tsv', True, args['threads'], args['version'])
-        convert('v1/test_public.json.gz', 'test_public.tsv', True, args['threads'], args['version'])
+        convert('v1/train.json.gz', 'train.tsv', False, args['threads'], args['version'], args['ratio'])
+        convert('v1/dev.json.gz', 'dev.tsv', False, args['threads'], args['version'], args['ratio'])
+        convert('v1/test.json.gz', 'test.tsv', True, args['threads'], args['version'], args['ratio'])
+        convert('v1/test_public.json.gz', 'test_public.tsv', True, args['threads'], args['version'], args['ratio'])
 
     else:
-        convert('v2/train.json.gz', 'train.tsv', False, args['threads'], args['version'])
-        convert('v2/dev.json.gz', 'dev.tsv', False, args['threads'], args['version'])
-        convert('v2/test.json.gz', 'test.tsv', True, args['threads'], args['version'])
-        convert('v2/test_public.json.gz', 'test_public.tsv', True, args['threads'], args['version'])
+        # convert('v2/train.json.gz', 'train.tsv', False, args['threads'], args['version'], args['ratio'])
+        # convert('v2/dev.json.gz', 'dev.tsv', False, args['threads'], args['version'], args['ratio'])
+        convert('v2/test.json.gz', 'test.tsv', True, args['threads'], args['version'], args['ratio'])
+        convert('v2/test_public.json.gz', 'test_public.tsv', True, args['threads'], args['version'], args['ratio'])
 
