bb08cb7a73a6a962872e04bea51c4da16df06ec6
zlsh80826 <zlsh80826@gmail.com>
2018-07-24 11:25:30 +0800

    update evaluation script to support both v1 and v2

--- Evaluation/eval.sh
+++ Evaluation/eval.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-set -xe
-export PYTHONPATH=../data
+if [ $# -ne 1 ]; then
+	echo 'Usage: sh eval.sh <version>'
+else
+    if [ $1 == 'v1' ]; then
+        set -xe
+        export PYTHONPATH=../data
+        cd ../script
+        echo 'Start to test ...'
+        python3.6 train.py --model ../model/pm.model --test ../data/test.tsv
+        cp ../model/pm.model_out.json ../output.json
 
-cd ../script
-echo 'Start to test ...'
-python3.6 train.py --model ../model/pm.model --test ../data/test.tsv
-cp ../model/pm.model_out.json ../output.json
+        echo 'Start to evaluate ...'
+        cd ../Evaluation
+        sh run.sh sample_test_data/dev_as_references.json ../output.json
+    elif [ $1 == 'v2' ]; then 
+        set -xe
+        export PYTHONPATH=../data
+        cd ../script
+        echo 'Start to test ...'
+        python3.6 train.py --model ../model/pm.model --test ../data/test.tsv
+        cp ../model/pm.model_out.json ../output.json
 
-# echo 'Start to evaluate ...'
-cd ../Evaluation
-sh run.sh sample_test_data/dev_as_references.json ../output.json
+        echo 'Start to evaluate ...'
+        cd ../Evaluation
+        sh run.sh sample_test_data/v2_dev.json ../output.json
+    else
+        echo '<version> can be only "v1" or "v2"'
+    fi
+fi
+
