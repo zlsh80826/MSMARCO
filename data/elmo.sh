2a92036a10d417dcd576c3e7f6b01e11bd9d071c
zlsh80826 <zlsh80826@gmail.com>
2018-08-02 20:41:31 +0800

    use gpu to generate elmo embedding

--- data/elmo.sh
+++ data/elmo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3.6 elmo.py
-allennlp elmo --all --batch-size 32768 vocabs.txt elmo_embedding.bin
+allennlp elmo --all --batch-size 10000 --cuda-device 0 vocabs.txt elmo_embedding.bin
