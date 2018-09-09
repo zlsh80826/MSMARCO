ab65fb9a4edee20e1b2700517237670ac0e9c485
zlsh80826 <zlsh80826@gmail.com>
2018-09-09 19:11:13 +0800

    better progress bar and separator

--- data/convert_msmarco.py
+++ data/convert_msmarco.py
@@ -1,15 +1,19 @@
+from utils import smith_waterman
+import multiprocessing
 from tqdm import tqdm
+import numpy as np
+import argparse
+import random
+import bisect
 import gzip
 import json
 import nltk
-import re
-import bisect
-import numpy as np
-import multiprocessing
 import time
-import argparse
-import random
-from utils import smith_waterman
+import re
+
+YesNoHead = ['is', 'are', 'was', 'were', 'do', 'does', 'did',
+             'can', 'could', 'should', 'has', 'have', 'may', 
+             'might', 'am', 'will', 'would']
 
 def preprocess(s):
     return s.replace("''", '" ').replace("``", '" ')
@@ -17,11 +21,12 @@ def preprocess(s):
 def tokenize(s, context_mode=False ):
     nltk_tokens=[t.replace("''", '"').replace("``", '"') for t in nltk.word_tokenize(s)]
     additional_separators = (
-        "-", "\u2212", "\u2014", "\u2013", "/", "~", '"', "'", "\u201C", "\u2019", "\u201D", "\u2018", "\u00B0")
+        "-", "\u2212", "\u2014", "\u2013", "~", '"', "\u201C", "\u2019", "\u201D", "\u2018", "\u00B0")
+        # "-", "\u2212", "\u2014", "\u2013", "/", "~", '"', "'", "\u201C", "\u2019", "\u201D", "\u2018", "\u00B0")
     tokens = []
     for token in nltk_tokens:
         tokens.extend([t for t in (re.split("([{}])".format("".join(additional_separators)), token)
-                                   if context_mode else [token])])
+            if context_mode else [token])])
     assert(not any([t=='<NULL>' for t in tokens]))
     assert(not any([' ' in t for t in tokens]))
     assert (not any(['\t' in t for t in tokens]))
@@ -34,22 +39,22 @@ def process(i, j, is_test):
     p = j['passages']
     outputs = []
 
-    YesNoHead = ['is', 'are', 'was', 'were', 'do', 'does', 'did',
-                 'can', 'could', 'should', 'has', 'have', 'may', 
-                 'might', 'am', 'will', 'would']
 
     query   = preprocess(j['query'])
     qtokens =  trim_empty(tokenize(query))
-    yesno = False
+    yesno   = False
+
     if qtokens[0].lower() in YesNoHead:
         yesno = True
 
-    temp = ' '.join(pp['passage_text'] for pp in p)
+    context = ' '.join(pp['passage_text'] for pp in p)
+
     if yesno:
-        temp += ' Yes No'
-    context = preprocess(temp)
+        context = 'Yes No ' + context
+    context = preprocess(context)
 
     ctokens = trim_empty(tokenize(context, context_mode=True))
+
     normalized_context = ' '.join(ctokens)
     nctokens = normalized_context.split()
 
@@ -126,23 +131,17 @@ def convert(file_name, outfile, is_test, threads, version, ratio):
     else:
         raise NotImplementedError
 
-    with multiprocessing.Pool(threads) as pool:
-        results = []
-        for idx, d in enumerate(tqdm(data)):
-            result = pool.apply_async(process, args=(idx, d, is_test))
-            results.append(result)
-        pool.close()
-        pool.join()
-        
     outputs = []
-
-    for result in results:
-        for output in result.get():
-            outputs.append(output)            
+    with multiprocessing.Pool(threads) as pool:
+        outputs = []
+        for output in tqdm(pool.imap(process, zip(data, repeat(is_test))), total=len(data)):
+            for o in output:
+                outputs.append(o)
 
     with open(outfile, 'w', encoding='utf-8') as out:
         for output in outputs:
             out.write("%s\n"%'\t'.join(output))
+
     end = time.perf_counter()
     print('Take', end - start, 'second.')
 
@@ -156,6 +155,6 @@ if __name__ == '__main__':
 
     convert(args.version + '/train.json.gz', 'train.tsv', False, args.threads, args.version, args.ratio)
     convert(args.version + '/dev.json.gz', 'dev.tsv', False, args.threads, args.version, args.ratio)
-    convert(args.version + '/test.json.gz', 'test.tsv', True, **var)
+    convert(args.version + '/test.json.gz', 'test_nltk.tsv', True, **var)
     convert(args.version + '/test_public.json.gz', 'test_public.tsv', True, args.threads, args.version, args.ratio)
 
