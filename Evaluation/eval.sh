904d0bc668255d7fcd9758b8f9d740b9e493142a
zlsh80826 <zlsh80826@gmail.com>
2018-11-05 23:52:04 +0800

    update evaluation script and answer

--- Evaluation/eval.sh
+++ Evaluation/eval.sh
@@ -8,23 +8,23 @@ else
         export PYTHONPATH=../data
         cd ../script
         echo 'Start to test ...'
-        python3.6 train.py --model ../model/pm.model --test ../data/test.tsv
+        \time --verbose python3.6 train.py --model ../model/pm.model --test ../data/test.tsv
         cp ../model/pm.model_out.json ../output.json
 
         echo 'Start to evaluate ...'
         cd ../Evaluation
-        sh run.sh sample_test_data/dev_as_references.json ../output.json
+        sh run.sh answer/dev_as_references.json ../output.json
     elif [ $1 == 'v2' ]; then 
         set -xe
         export PYTHONPATH=../data
         cd ../script
         echo 'Start to test ...'
-        python3.6 train.py --model ../model/pm.model --test ../data/test.tsv
+        \time --verbose python3.6 train.py --model ../model/pm.model --test ../data/test.tsv
         cp ../model/pm.model_out.json ../output.json
 
         echo 'Start to evaluate ...'
         cd ../Evaluation
-        sh run.sh sample_test_data/v2_dev.json ../output.json
+        sh run.sh answer/v2_dev_noans.json ../output.json
     else
         echo '<version> can be only "v1" or "v2"'
     fi
