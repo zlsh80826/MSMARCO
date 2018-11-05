138f03af2d0aacc5b91d654724c0d5a5b707fbc0
zlsh80826 <zlsh80826@gmail.com>
2018-11-05 23:41:22 +0800

    update best configuration

--- script/config.py
+++ script/config.py
@@ -1,30 +1,33 @@
 data_config = {
-    'word_size' : 16,
+    'word_size'            : 16,
     'word_count_threshold' : 10,
     'char_count_threshold' : 50,
-    'pickle_file' : '../data/vocabs.pkl',
+    'pickle_file'          : '../data/vocabs.pkl',
+    'elmo_embedding'       : '../data/elmo_embedding.bin',
+    'glove_embedding'      : '../data/glove.840B.300d.txt'
 }
 
 model_config = {
+    'elmo_dim'          : 1024,
     'hidden_dim'     	: 300,
     'char_convs'     	: 300,
     'char_emb_dim'   	: 16,
-    'dropout'        	: 0.2,
+    'dropout'        	: 0.25,
     'highway_layers' 	: 2,
     'two_step'          : True,
     'use_cudnn'         : True,
 }
 
 training_config = {
-    'minibatch_size'    : 4096,     # in samples when using ctf reader, per worker
-    'epoch_size'        : 80115,    # in sequences, when using ctf reader
-    'log_freq'          : 2000,     # in minibatchs
+    'minibatch_size'    : 1800,     # in samples when using ctf reader, per worker
+    'epoch_size'        : 473093,   # in sequences, when using ctf reader
+    'log_freq'          : 10000,    # in minibatchs
     'max_epochs'        : 20,
-    'lr'                : 2,
+    'lr'                : 1,
     'train_data'        : 'train.ctf',  # or 'train.tsv'
     'val_data'          : 'dev.ctf',
     'val_interval'      : 1,       # interval in epochs to run validation
-    'stop_after'        : 2,       # num epochs to stop if no CV improvement
+    'stop_after'        : 1,       # num epochs to stop if no CV improvement
     'minibatch_seqs'    : 16,      # num sequences of minibatch, when using tsv reader, per worker
     'distributed_after' : 0,       # num sequences after which to start distributed training
 }
--- script/run.sh
+++ script/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PYTHONPATH=../data
-python3.6 train.py -datadir ../data -outputdir ../model -logdir log/
+\time --verbose mpirun -n 4 python3.6 train.py -datadir ../data -outputdir ../model -logdir log/
