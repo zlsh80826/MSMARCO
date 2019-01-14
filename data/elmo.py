3bd865999f5ff5fe7b3753c5d7ba3ac446cd0974
zlsh80826 <zlsh80826@gmail.com>
2019-01-14 17:05:48 +0800

    new elmo

--- data/elmo.py
+++ data/elmo.py
@@ -6,7 +6,7 @@ known, vocab, chars = pickle.load(open('vocabs.pkl', 'rb'))
 
 with open('vocabs.txt', 'w') as file:
     for v in vocab:
-        if "/" not in v:
+        if '/' not in v and ';' not in v and v is not '' and v  != '.':
             file.write(v + '\n')
 
 
--- data/elmo.sh
+++ data/elmo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3.6 elmo.py
-allennlp elmo --all --batch-size 10000 --cuda-device 0 vocabs.txt elmo_embedding.bin
+allennlp elmo --all --batch-size 12000 --cuda-device 0 --use-sentence-keys vocabs.txt elmo_embedding.bin 
